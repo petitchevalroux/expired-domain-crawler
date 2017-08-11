bf0903a5fa0c9a3edb7472f2cb89ace3953051fe
Patrick Poulain <petitchevalroux@free.fr>
2017-08-12 00:31:52 +0200

    Create crawler

---
+++ src/crawler.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const Promise = require("bluebird");
+
+class Crawler {
+    constructor(options) {
+        this.fifoRepository = options.fifoRepository;
+        this.filterStream = options.filterStream;
+        this.downloadStream = options.downloadStream;
+        this.extractStream = options.extractStream;
+    }
+
+    addUrl(url) {
+        this
+                .getUnfilteredUrlsFifo()
+                .then((fifo) => {
+                    return fifo.write(url);
+                });
+    }
+
+    run() {
+        const self = this;
+        Promise.all([
+            this.getUnfilteredUrlsFifo,
+            this.getFilteredUrlsFifo
+        ])
+        .then((fifos) => {
+            fifos[0]
+                .pipe(self.filterStream)
+                .pipe(fifos[1])
+                .pipe(self.downloadStream)
+                .pipe(self.extractStream)
+                .pipe(fifos[0]);
+        });
+    }
+
+    getUnfilteredUrlsFifo() {
+        return this.fifoRepository.get("urls:unfiltered");
+    }
+
+    getFilteredUrlsFifo() {
+        return this.fifoRepository.get("urls:filtered");
+    }
+}
+
+module.exports = Crawler;
