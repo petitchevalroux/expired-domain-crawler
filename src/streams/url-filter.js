4252f0f59ab5b00fad9c5a3d3ae258c110607a22
Patrick Poulain <petitchevalroux@free.fr>
2017-08-21 22:00:06 +0200

    Filter non http urls

--- src/streams/url-filter.js
+++ src/streams/url-filter.js
@@ -10,36 +10,45 @@ class UrlFilterStream extends Transform {
         this.urlRepository = options.urlRepository;
     }
     _transform(chunk, encoding, callback) {
-        const url = chunk.toString();
-        const self = this;
-        self.urlRepository
-            .getByUrlOrCreate(url)
-            .then((urlObject) => {
-                return urlObject
-                    .isToDownload()
-                    .then((toDownload) => {
-                        if (toDownload) {
-                            return urlObject
-                                .setLastDownloaded(new Date())
-                                .then((urlObject) => {
-                                    return self.urlRepository.update(
-                                        urlObject.id,
-                                        urlObject
-                                    );
-                                })
-                                .then(() => {
-                                    callback(null, url);
-                                    return toDownload;
-                                });
-                        } else {
-                            callback();
-                        }
-                        return toDownload;
-                    });
-            })
-            .catch((err) => {
-                callback(err);
-            });
+        try {
+            const url = chunk.toString()
+                .trim();
+            if (url.substring(0, 4) !== "http") {
+                return callback();
+            }
+            const self = this;
+            self.urlRepository
+                .getByUrlOrCreate(url)
+                .then((urlObject) => {
+                    return urlObject
+                        .isToDownload()
+                        .then((toDownload) => {
+                            if (toDownload) {
+                                return urlObject
+                                    .setLastDownloaded(new Date())
+                                    .then((urlObject) => {
+                                        return self.urlRepository
+                                            .update(
+                                                urlObject.id,
+                                                urlObject
+                                            );
+                                    })
+                                    .then(() => {
+                                        callback(null, url);
+                                        return toDownload;
+                                    });
+                            } else {
+                                callback();
+                            }
+                            return toDownload;
+                        });
+                })
+                .catch((err) => {
+                    callback(err);
+                });
+        } catch (err) {
+            return callback(err);
+        }
     }
 }
 module.exports = UrlFilterStream;
