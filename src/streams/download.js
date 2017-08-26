77bdb98f5640a11d6ca7c22b274788e0212daa14
Patrick Poulain <petitchevalroux@free.fr>
2017-08-26 22:15:01 +0200

    Support null result in download stream

--- src/streams/download.js
+++ src/streams/download.js
@@ -13,20 +13,18 @@ class DownloadStream extends Transform {
         this.httpErrorStream = options.httpErrorStream || false;
         this.urlRepository = options.urlRepository;
     }
+
     _transform(chunk, encoding, callback) {
         const self = this;
         super._transform(chunk, encoding, (err, result) => {
-            const url = chunk.toString();
             if (err) {
                 // Don't stop on http error
                 if (err instanceof HttpError) {
                     if (!self.httpErrorStream) {
                         return callback();
                     }
-                    const httpError = Object.assign({}, err);
-                    httpError.url = url;
                     return self.httpErrorStream.write(
-                        httpError,
+                        Object.assign({}, err),
                         "",
                         (err) => {
                             callback(err);
@@ -36,6 +34,10 @@ class DownloadStream extends Transform {
                     return callback(err);
                 }
             }
+            if (!result) {
+                return callback(null);
+            }
+            const url = result.input;
             self.urlRepository
                 .getByUrlOrCreate(url)
                 .then((urlObject) => {
