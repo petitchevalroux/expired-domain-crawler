671b176e7bea7255c74436004aed67d70c3297dc
Patrick Poulain <petitchevalroux@free.fr>
2017-09-29 19:13:38 +0200

    Filter binary files

--- src/streams/url-filter.js
+++ src/streams/url-filter.js
@@ -4,7 +4,8 @@ const {
     } = require("stream"),
     path = require("path"),
     dateUtils = require(path.join(__dirname, "..", "utils", "date")),
-    urlModule = require("url");
+    urlModule = require("url"),
+    Promise = require("bluebird");
 class UrlFilterStream extends Transform {
     constructor(options) {
         options.readableObjectMode = true;
@@ -20,10 +21,8 @@ class UrlFilterStream extends Transform {
         try {
             const url = chunk.toString()
                 .trim();
-            const hostname = urlModule.parse(url)
-                .hostname;
             const self = this;
-            this.filterUrl(hostname, url)
+            this.filterUrl(urlModule.parse(url))
                 .then((url) => {
                     return callback(null, url);
                 })
@@ -35,41 +34,51 @@ class UrlFilterStream extends Transform {
         }
     }
 
-    filterUrl(hostname, url) {
-        const self = this;
-        return this.domainRepository
-            .getByHostnameOrCreate(hostname)
-            .then((domainObject) => {
-                if (domainObject.downloadedUrls >= self.maxUrlsPerDomain) {
-                    return undefined;
-                }
-                return self
-                    .domainRepository
-                    .increment(domainObject.id, "downloadedUrls", 1)
-                    .then(() => {
-                        return self
-                            .urlRepository
-                            .getByUrlOrCreate(url);
-                    })
-                    .then((urlObject) => {
-                        if (urlObject.lastDownloaded >
-                            (dateUtils.getTimestamp() - self.urlsDownloadGrace)
-                        ) {
-                            return undefined;
-                        }
-                        return self
-                            .urlRepository
-                            .update(
-                                urlObject.id, {
-                                    "lastDownloaded": dateUtils
-                                        .getTimestamp()
-                                }
-                            )
-                            .then(() => {
-                                return urlObject.url;
-                            });
-                    });
-            });
+    filterUrl(opts) {
+        try {
+            const self = this,
+                hostname = opts.hostname,
+                url = opts.href;
+            if ([".jpg", ".jpeg", ".png", ".gif", ".bmp", ".pdf"]
+                .indexOf(path.extname(opts.pathname)) > -1) {
+                return Promise.resolve();
+            }
+            return this.domainRepository
+                .getByHostnameOrCreate(hostname)
+                .then((domainObject) => {
+                    if (domainObject.downloadedUrls >= self.maxUrlsPerDomain) {
+                        return undefined;
+                    }
+                    return self
+                        .domainRepository
+                        .increment(domainObject.id, "downloadedUrls", 1)
+                        .then(() => {
+                            return self
+                                .urlRepository
+                                .getByUrlOrCreate(url);
+                        })
+                        .then((urlObject) => {
+                            if (urlObject.lastDownloaded >
+                                (dateUtils.getTimestamp() - self.urlsDownloadGrace)
+                            ) {
+                                return undefined;
+                            }
+                            return self
+                                .urlRepository
+                                .update(
+                                    urlObject.id, {
+                                        "lastDownloaded": dateUtils
+                                            .getTimestamp()
+                                    }
+                                )
+                                .then(() => {
+                                    return urlObject.url;
+                                });
+                        });
+                });
+        } catch (err) {
+            return Promise.reject(err);
+        }
     }
 
     filterError(err, callback) {
